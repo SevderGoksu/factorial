178b50a3af8a84c3df0b4e26944c7bc57c6baf00
SevderGoksu <50488498+SevderGoksu@users.noreply.github.com>
2019-05-13 13:18:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaktoriyelHesabi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Bir sayı giriniz ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sonuç= {0} ", facturial(number));
+            Console.ReadKey();
+        }
+        public static int facturial(int number)
+        {
+            if (number <= 1)
+            { return number = 1; }
+            else
+            { return number * facturial(number - 1); }
+           
+        }
+    }
+}
